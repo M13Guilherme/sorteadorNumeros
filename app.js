005ffaf9c11cb6a6dae524ba4afd6e374246ec85
M13Guilherme <mikebrow19@hotmail.com>
2024-05-07 18:55:35 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-function sortear(){
+function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
-    if (de >= ate) {
-        alert('O campo "Do Número" dever ser inferior ao campo "Até o Número". Verifique o número inserido!');
-        return;
-    }
-
     let sorteados = [];
     let numero;
 
-    for(let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatório(de, ate);
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
 
         while (sorteados.includes(numero)) {
-            numero = obterNumeroAleatório(de, ate);
-        }
+            numero = obterNumeroAleatorio(de, ate);
 
+        }
         sorteados.push(numero);
     }
 
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
     alterarStatusBotao();
 }
 
-function obterNumeroAleatório (min, max){
+function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -45,6 +40,6 @@ function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
-    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
     alterarStatusBotao();
 }
